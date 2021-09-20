7d29e6706c9539e470334826d4ddc33e59facf51
Oussama Alouat <al317257@uji.es>
2021-09-20 16:19:32 +0200

    Add query to wikipedia api

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,30 @@
+import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 
 const Search = () => {
   const [term, setTerm] = useState('');
+  const [results, setResults] = useState([])
+
+  console.log(results)
+
   useEffect(()=> {
-    console.log('iM ON USE EFECT')
+    const search = async() => {
+      const  { data }  = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term
+        }
+      });
+
+      setResults(data.query.search)
+    };
+
+    if (term) {
+      search();
+    }
   }, [term])
 
   return (
