6f65ff9b682ae9552f6f50ffcb2c66faffd15caf
Oussama Alouat <al317257@uji.es>
2021-09-20 11:37:43 +0200

    Add Accordion component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import Accordion from './components/Accordion';
+
+const items = [
+  {
+    title: 'What is React ?',
+    content: 'React is a front end javascript framework'
+  },
+  {
+    title: 'Why use React',
+    content: 'React is a a favourite framework'
+  },
+  {
+    title: 'How do you use React',
+    content: 'You use React by creating components'
+  }
+]
 
 export default () => {
-  return <h1>Hi from App</h1>
+  return (
+    <div>
+      <h1>Hi from App</h1>
+      <Accordion items={items} />
+    </div>
+    )
 }
---
+++ src/components/Accordion.js
@@ -0,0 +1,8 @@
+import react from 'react';
+
+const Accordion = (props) => {
+  const { items } = props;
+  return <h1>HI Accordion {items.length}</h1>
+}
+
+export default Accordion;
