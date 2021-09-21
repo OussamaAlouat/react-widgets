b7630b62d13c255327e9027ed6f53b5d86d4d00e
Oussama Alouat <al317257@uji.es>
2021-09-21 11:19:05 +0200

    Add state to save Dropdown selections

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Dropdown from './components/Dropdown';
-import Search from './components/Search'
+import Search from './components/Search';
+
 const items = [
   {
     title: 'What is React ?',
@@ -33,9 +34,15 @@ const options = [
 ];
 
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
-      <Dropdown options={options} />
+      <Dropdown
+        selected={ selected }
+        options={ options }
+        onSelectedChange={setSelected}
+      />
     </div>
     )
 }
